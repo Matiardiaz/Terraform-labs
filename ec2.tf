6983d3f39eb71a51c56bd99ef6398c29136dc570
Matias <matiasrdiaz@gmail.com>
2023-04-30 17:48:21 +0200

    nueva ami y comentada la rds

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 # Creating 1st EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance" {
-  ami                         = "ami-087c17d1fe0178315"
-  instance_type               = "t2.micro"
+  # ami                         = "ami-087c17d1fe0178315"
+  ami                         = "ami-064e65efdd62efb36"
+  instance_type               = "t3.micro"
   # count                       = 1
   #key_name                    = "tests"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
@@ -16,10 +17,11 @@ resource "aws_instance" "demoinstance" {
 
 # Creating 2nd EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance1" {
-  ami                         = "ami-087c17d1fe0178315"
-  instance_type               = "t2.micro"
+  # ami                         = "ami-087c17d1fe0178315"
+  ami                         = "ami-064e65efdd62efb36"
+  instance_type               = "t3.micro"
   # count                       = 1
-  key_name                    = "tests"
+  # key_name                    = "tests"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-2.id}"
   associate_public_ip_address = true
