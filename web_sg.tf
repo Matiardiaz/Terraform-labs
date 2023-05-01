f01e12db29c31aa0fc2a41f2d01e334e1f937a8d
Matias <matiasrdiaz@gmail.com>
2023-05-01 13:56:58 +0200

    editado gitignore

--- web_sg.tf
+++ web_sg.tf
@@ -3,21 +3,23 @@ resource "aws_security_group" "demosg" {
   vpc_id = "${aws_vpc.demovpc.id}"
 
   # Inbound Rules
-  # HTTP access from anywhere
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  # HTTP access from Load balancer only
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    referenced_security_group_id = aws_security_group.alb_sg.id
+    # cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  # # HTTPS access from anywhere
-  # ingress {
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  # HTTPS access from load balancer only
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    source_security_group_id = aws_security_group.alb_sg.id
+    # cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # SSH access from anywhere
   ingress {
